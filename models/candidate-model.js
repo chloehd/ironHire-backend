bfb87fafd94e80309cb1b6d8943e7e5766c69788
chloehd <chloehipeau@gmail.com>
2019-12-15 23:17:09 +0100

    change name of the collection and delete a page

--- models/candidate-model.js
+++ models/candidate-model.js
@@ -1,49 +1,56 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const candidateSchema = new Schema({
+const candidateSchema = new Schema(
+  {
     candidate_pic: { type: String },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: /^.+@.+\..+$/,
-      }, 
+      type: String,
+      required: true,
+      unique: true,
+      match: /^.+@.+\..+$/
+    },
     telephone_number: { type: Number },
-    employment_status: { 
-        type: String,
-        enum: ["searching", "open to offers", "employed"],
-     },
-    experience: [{
+    employment_status: {
+      type: String,
+      enum: ["searching", "open to offers", "employed"]
+    },
+    experience: [
+      {
         companyName: { type: String, required: true },
         duration: { type: String, required: true },
-        tasks: { type: String, required: true },
-    }],
-    education: [{
+        tasks: { type: String, required: true }
+      }
+    ],
+    education: [
+      {
         studyName: { type: String, required: true },
         schoolName: { type: String, required: true },
-        duration: { type: String, required: true },
-    }],
+        duration: { type: String, required: true }
+      }
+    ],
     languages: [{ type: String, required: true }],
     skills: { type: String },
     verified: {
-        type: Boolean,
-        default: false,
-        required: true,
+      type: Boolean,
+      default: false,
+      required: true
     },
     encryptedPassword: { type: String },
     role: {
-        type: String,
-        enum: ["candidate", "admin", "recruiter", "association"],
-        required: true,
-        default: "candidate",
+      type: String,
+      enum: ["candidate", "admin", "recruiter", "association"],
+      required: true,
+      default: "candidate"
     }
-}, {
-        timestamps: true
-    });
+  },
+  {
+    timestamps: true
+  }
+);
 
-const Candidate = mongoose.model("cvs", candidateSchema);
+const Candidate = mongoose.model("candidates", candidateSchema);
 
 module.exports = Candidate;
